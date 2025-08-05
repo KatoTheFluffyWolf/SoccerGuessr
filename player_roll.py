4d4dcbed458cb8a671f3a22343b08c4ce457fa21
Kato <katotinminecraft@gmail.com>
2025-08-05 13:33:41 +0700

    Update player_roll.py

--- player_roll.py
+++ player_roll.py
@@ -12,7 +12,6 @@ SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJ
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-@app.post("/daily/spin")
 def player_roll():
     today = datetime.now().strftime('%Y-%m-%d')
 
